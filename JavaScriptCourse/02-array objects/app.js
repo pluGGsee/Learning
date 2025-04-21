d1a1499acc0b05417e4e86ac62f5011f0d858410
Alex Buldin <pluggsee@yandex.ru>
2025-04-21 13:35:27 +0300

    Done App

--- JavaScriptCourse/02-array objects/app.js
+++ JavaScriptCourse/02-array objects/app.js
@@ -54,12 +54,12 @@ const notes = [
 ]
 
 function render() {
-  //   for (let i = 0; i < notes.length; i++) {
-  //     listElement.insertAdjacentHTML("beforeend", getNoteTemplate(notes[i]))
-  //   }
-
-  for (let note of notes) {
-    listElement.insertAdjacentHTML("beforeend", getNoteTemplate(note))
+  listElement.innerHTML = ""
+  if (notes.length === 0) {
+    listElement.innerHTML = "<p>Заметок пока нет</p>"
+  }
+  for (let i = 0; i < notes.length; i++) {
+    listElement.insertAdjacentHTML("beforeend", getNoteTemplate(notes[i], i))
   }
 }
 
@@ -73,19 +73,39 @@ createBtn.onclick = function () {
     title: inputElement.value,
     completed: false,
   }
-  listElement.insertAdjacentHTML("beforeend", getNoteTemplate(newNote))
+  notes.push(newNote)
+  render()
   inputElement.value = ""
 }
 
-function getNoteTemplate(note) {
+listElement.onclick = function (event) {
+  if (event.target.dataset.index) {
+    const index = parseInt(event.target.dataset.index)
+    const type = event.target.dataset.type
+
+    if (type === "toggle") {
+      notes[index].completed = !notes[index].completed
+    } else if (type === "remove") {
+      notes.splice(index, 1)
+    }
+
+    render()
+  }
+}
+
+function getNoteTemplate(note, index) {
   return `
       <li
         class="list-group-item d-flex justify-content-between align-items-center"
       >
-        <span>${note.title}</span>
+        <span class ="${
+          note.completed ? "text-decoration-line-through" : ""
+        }">${note.title}</span>
         <span>
-            <span class="btn btn-small btn-success">&check;</span>
-            <span class="btn btn-small btn-danger">&times;</span>
+            <span class="btn btn-small btn-${
+              note.completed ? "warning" : "success"
+            }" data-index="${index}" data-type="toggle">&check;</span>
+            <span class="btn btn-small btn-danger"data-type="remove" data-index="${index}">&times;</span>
         </span>
       </li>
       `
